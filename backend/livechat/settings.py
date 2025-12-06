995d6fdbca2555028604b05b414424307b71263e
Pradip Thapa <thepradipthapa.404@gmail.com>
2025-12-06 19:41:57 +0545

    feat: add Custom User model

--- backend/livechat/settings.py
+++ backend/livechat/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #  Thirt party
+    'rest_framework',
+    
+    # Apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Custom User Model
+AUTH_USER_MODEL = "accounts.User"
