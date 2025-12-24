33e7c398d3aea23fcb6fdb180168dffacff1f3d5
Pradip Thapa <thepradipthapa.404@gmail.com>
2025-12-24 22:30:59 +0545

    feat: add chat models

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from backend.chat.models import Conversation, ConversationParticipant, Message
 
 # Register your models here.
+admin.site.register(Conversation)
+admin.site.register(ConversationParticipant)
+admin.site.register(Message)
--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + '->' + self.name
+
+class ConversationParticipant(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE , related_name='participants')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    joined_at = models.DateTimeField(auto_now_add=True)
+    
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.content
